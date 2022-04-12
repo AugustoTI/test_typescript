9a02b7033d751258c1e1e2c1da682adc2d6a4a24
AugustoTI <augustocesarti2021@gmail.com>
2022-04-12 12:49:59 -0300

    introdução básica dos testes

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,9 @@
+it('isso é um teste do teste', () => {
+  const number = 1;
+  expect(number).toBe(1);
+});
+
+test('isso é um outro teste', () => {
+  const name = 'Luiz';
+  expect(name).toBe('Luiz');
+});
