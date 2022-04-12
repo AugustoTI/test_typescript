fca81ca105240bcf883df724aac54c8905a0a02f
AugustoTI <augustocesarti2021@gmail.com>
2022-04-12 20:55:22 -0300

    testing 'spyOn' in Jest

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,9 +1,27 @@
-it('isso é um teste do teste', () => {
-  const number = 1;
-  expect(number).toBe(1);
-});
+import { Persistency } from './persistency';
 
-test('isso é um outro teste', () => {
-  const name = 'Luiz';
-  expect(name).toBe('Luiz');
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefinied', () => {
+    // System under test
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log once ', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(sut, 'saveOrder');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Seu pedido foi recebido com sucesso" ', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith(
+      'Seu pedido foi recebido com sucesso',
+    );
+  });
 });
